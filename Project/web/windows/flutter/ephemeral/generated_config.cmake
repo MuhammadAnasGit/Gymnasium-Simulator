298ef4736d0c96700e50d4462572502f57e9f027
MuhammadAnasGit <157326893+MuhammadAnasGit@users.noreply.github.com>
2024-12-11 20:41:02 -0600

    Add files via upload

---
+++ Project/web/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Fluuter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Fluuter\\flutter"
+  "PROJECT_DIR=C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator"
+  "FLUTTER_ROOT=C:\\Fluuter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator"
+  "FLUTTER_TARGET=C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\1234\\Desktop\\Gymnasium Simulator\\gymnasium_simulator\\.dart_tool\\package_config.json"
+)
